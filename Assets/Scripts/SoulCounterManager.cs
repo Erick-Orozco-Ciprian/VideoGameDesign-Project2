463d57627860d396bb8ee4cc220a83239ff96238
sierranmorales <143547246+sierranmorales@users.noreply.github.com>
2024-04-01 21:23:18 -1000

    Added Soul Counter and Enemy Collision
    Added a "soul counter" that increments when player collides with soul enemy. Also added functionality that deletes instance of soul enemy on collision.

---
+++ Assets/Scripts/SoulCounterManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoulCounterManager : MonoBehaviour
+{
+    public int soulCount = 0;
+    public Text soulCounterText;
+
+    private void Start()
+    {
+        UpdateSoulCounterUI();
+    }
+
+    public void IncreaseSoulCount()
+    {
+        soulCount++; // Increment the soul count.
+        UpdateSoulCounterUI(); // Update the UI
+    }
+
+    private void UpdateSoulCounterUI()
+    {
+        // Update the text of the soulCounterText UI element.
+        soulCounterText.text = "Souls: " + soulCount;
+    }
+}
